ea04b1661fd8151d4b5a1d19dcf6ab46aa025725
Hardik <hardikrajpal@cse.iitb.ac.in>
2023-02-28 23:47:46 +0530

    deploy.py

--- deploy.py
+++ deploy.py
@@ -1,14 +1,21 @@
 from subprocess import Popen, PIPE
-
-p = Popen(['git', 'branch'], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+import subprocess
+deploybranch = 'gh-pages'
+p = Popen(['git', 'branch', '--show-current'], stdin=PIPE, stdout=PIPE, stderr=PIPE)
 output, err = p.communicate(b"input data that is passed to subprocess' stdin")
 rc = p.returncode
-# subprocess.call(['bash','deploy.sh'])
-# with open('cssfiles.txt','r') as f:
-#     ctnt = f.read().splitlines()
-#     ctnt.pop()
-# for fpath in ctnt:
-#     with open(f'src/{fpath}','w+') as f:
-#         styles = f.read()
-#         styles = styles.replace('/assets/','/VialRack/assets/')
-#         f.write(styles)
+output = output.decode('utf-8').strip()
+if(output!=deploybranch):
+    print('Not in deployment branch: ',deploybranch, '. Exiting...')
+    exit()
+
+subprocess.call(['bash','deploy.sh'])
+with open('cssfiles.txt','r') as f:
+    ctnt = f.read().splitlines()
+    ctnt.pop()
+for fpath in ctnt:
+    with open(f'src/{fpath}','w+') as f:
+        styles = f.read()
+        styles = styles.replace('/assets/','/VialRack/assets/')
+        f.write(styles)
+
