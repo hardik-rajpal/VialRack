9649c79bfafd3ca60998fcbf531dda90876b743e
Hardik <hardikrajpal@cse.iitb.ac.in>
2023-03-15 21:38:09 +0530

    blog htlm

--- src/app/pages/blog/blog.component.ts
+++ src/app/pages/blog/blog.component.ts
@@ -1,15 +1,27 @@
 import { Component } from '@angular/core';
 import { HrubService } from 'src/app/services/hrub.service';
-
+export interface blogPost{
+  title:string;
+  publishedLink:string;
+  tags:string[]
+  summary:string;
+}
 @Component({
   selector: 'app-blog',
   templateUrl: './blog.component.html',
   styleUrls: ['./blog.component.css']
 })
 export class BlogComponent {
+  allBlogPosts:blogPost[] = [];
+  dataLoaded:boolean = false;
+  filteredBlogPosts:blogPost[] = [];
+  activePost:blogPost|null = null;
   constructor(private hrubService:HrubService){
     this.hrubService.getBlogs().subscribe((data)=>{
       console.log(data)
+      this.allBlogPosts = data as blogPost[];
+      this.filteredBlogPosts = data as blogPost[]
+      this.dataLoaded = true;
     })
   }
 }
