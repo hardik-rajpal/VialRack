e4108ac69063070a5ab8e856e0d91fb675a9ea9b
Hardik <hardikrajpal@cse.iitb.ac.in>
2023-05-22 21:59:25 +0530

    script

--- deploy.ps1
+++ deploy.ps1
@@ -2,6 +2,3 @@ bash -c 'rm -rf .angular node_modules/.cache dist'
 git checkout deploy
 git merge master
 bash -c 'rm main.*.js polyfills.*.js runtime.*.js styles.*.css index.html'    
-ng build --base-href https://hardik-rajpal.github.io/ISID-EPU/;
-bash -c 'mv dist/isid-epu/* .';
-bash -c 'cp index.html 404.html; rm -rf assets; cp -r src/assets assets';
--- deploy2.ps1
+++ deploy2.ps1
@@ -1,3 +1,6 @@
+ng build --base-href https://hardik-rajpal.github.io/ISID-EPU/;
+bash -c 'mv dist/isid-epu/* .';
+bash -c 'cp index.html 404.html; rm -rf assets; cp -r src/assets assets';
 git add -A;
 $msg = bash -c "git log -2 --pretty=%B | sed -n '3 p'"
 git commit -m "$msg";
