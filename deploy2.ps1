6ea0f3645593635ed3333e6e31dae00c37f617b7
Hardik <hardikrajpal@cse.iitb.ac.in>
2023-05-29 19:41:27 +0530

    script fix.

--- deploy2.ps1
+++
@@ -1,8 +0,0 @@
-ng build --base-href https://hardik-rajpal.github.io/VialRack/;
-bash -c 'mv dist/vial-rack/* .';
-bash -c 'cp index.html 404.html; rm -rf assets; cp -r src/assets assets';
-git add -A;
-$msg = bash -c "git log -2 --pretty=%B | sed -n '3 p'"
-git commit -m "$msg";
-git push origin deploy --force;
-git checkout master;
